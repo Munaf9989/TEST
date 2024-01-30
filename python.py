37bc06869497d9429a2fa61784c36dd34325f005
Munaf9989 <ec2-user@ip-172-31-47-251.us-east-2.compute.internal>
2024-01-30 08:32:46 +0000

    This is the third commit

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
 s This is the first commite
 This is the second commit
 This is the Third commite done in github
+This i sthe forth commit done in child local branch
+This is the fifth commite done in main local repo
