57390981cb30975d01a1f30c8207b92fb9a02a1f
Munaf9989 <142387804+Munaf9989@users.noreply.github.com>
2024-02-29 12:44:58 +0530

    Delete python.py

--- python.py
+++
@@ -1,6 +0,0 @@
-This is the first commite
-This is the second commit
-This is the Third commite done in github
-This i sthe forth commit done in child local branch
-This is the fifth commite done in main local repo
-This is the sixth commite done in child local repo
